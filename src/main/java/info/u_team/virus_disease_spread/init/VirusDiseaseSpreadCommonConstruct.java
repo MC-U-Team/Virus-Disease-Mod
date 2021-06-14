0886c7de4d67a18dd774fd42d4096b973533fdd9
HyCraftHD <hycrafthd@live.de>
2021-06-15 01:33:24 +0200

    Register deferred register classes

--- src/main/java/info/u_team/virus_disease_spread/init/VirusDiseaseSpreadCommonConstruct.java
+++ src/main/java/info/u_team/virus_disease_spread/init/VirusDiseaseSpreadCommonConstruct.java
@@ -2,6 +2,7 @@ package info.u_team.virus_disease_spread.init;
 
 import info.u_team.u_team_core.api.construct.Construct;
 import info.u_team.u_team_core.api.construct.IModConstruct;
+import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.virus_disease_spread.VirusDiseaseSpreadMod;
 import info.u_team.virus_disease_spread.config.CommonConfig;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -13,6 +14,10 @@ public class VirusDiseaseSpreadCommonConstruct implements IModConstruct {
 	@Override
 	public void construct() {
 		ModLoadingContext.get().registerConfig(Type.COMMON, CommonConfig.CONFIG);
+		
+		BusRegister.registerMod(VirusDiseaseSpreadEffects::registerMod);
+		BusRegister.registerMod(VirusDiseaseSpreadItems::registerMod);
+		BusRegister.registerMod(VirusDiseaseSpreadPotions::registerMod);
 	}
 	
 }
