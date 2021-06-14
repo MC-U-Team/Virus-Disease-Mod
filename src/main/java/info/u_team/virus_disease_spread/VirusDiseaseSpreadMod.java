ed2d4567202f39168019c061c4705e94e6ad6068
HyCraftHD <hycrafthd@live.de>
2021-06-15 01:32:38 +0200

    Remove unused import

--- src/main/java/info/u_team/virus_disease_spread/VirusDiseaseSpreadMod.java
+++ src/main/java/info/u_team/virus_disease_spread/VirusDiseaseSpreadMod.java
@@ -2,10 +2,7 @@ package info.u_team.virus_disease_spread;
 
 import info.u_team.u_team_core.util.annotation.AnnotationManager;
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
-import info.u_team.virus_disease_spread.config.CommonConfig;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig.Type;
 
 @Mod(VirusDiseaseSpreadMod.MODID)
 public class VirusDiseaseSpreadMod {
